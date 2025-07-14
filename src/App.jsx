e17179d049fd271f3c9d9cb699b3d178ca9e1b86
Pavithra S <pavithra.s2023ece@sece.ac.in>
2025-07-14 14:29:52 +0530

    Day 9 of Springboot Internship - react + vite setup and login page

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Login from "./components/Login"
+import Register from "./components/Register"
+//import './index.css';
+
+function App() {
+  return(
+    <div>
+      <Login />
+      <Register />
+      
+    </div>
+  )
+}
